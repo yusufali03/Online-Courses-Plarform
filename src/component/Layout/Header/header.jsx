191a7960dc3858cbcf6ead5d1268cdc0b652ad47
leasiond <1acaunt002@gmail.com>
2025-02-17 12:49:11 +0500

    header updated

--- src/component/Layout/Header/header.jsx
+++ src/component/Layout/Header/header.jsx
@@ -98,10 +98,10 @@ const Header = () => {
                 <FaCaretDown />
               </CDropdownToggle>
               <CDropdownMenu className="dropdown-menu">
-                <CDropdownItem href="/courses-list/grid">
+                <CDropdownItem href="/courses-list/item">
                   {t("courses_list")}
                 </CDropdownItem>
-                <CDropdownItem href="#">{t("courses_grid")}</CDropdownItem>
+                <CDropdownItem href="/courses-list/grid">{t("courses_grid")}</CDropdownItem>
                 <CDropdownItem href="#">{t("courses_details")}</CDropdownItem>
                 <CDropdownItem href="#">{t("courses_sidebar")}</CDropdownItem>
               </CDropdownMenu>
